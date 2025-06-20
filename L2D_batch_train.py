32f58ede0b70abdaa637cad13856e9509cb2fefc
LRasanjalee <rlokesha7@gmail.com>
2025-06-20 19:22:21 +0930

    removed image saving

--- L2D_batch_train.py
+++ L2D_batch_train.py
@@ -540,17 +540,17 @@ def add_mask(input_mask_dir,output_mask_dir,base_video_dir, video_name, frame_na
         
         #------------------Save Images------------------------------
         
-        os.makedirs(output_mask_dir, exist_ok=True)
-        plt.figure(figsize=(9, 6))
-        plt.title(f"frame {input_frame_idx}")
-        plt.imshow(Image.open(os.path.join(base_video_dir, video_name, f"{frame_names[input_frame_idx]}.jpg")))
-        show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
-        print(out_mask_logits.shape)
+        # os.makedirs(output_mask_dir, exist_ok=True)
+        # plt.figure(figsize=(9, 6))
+        # plt.title(f"frame {input_frame_idx}")
+        # plt.imshow(Image.open(os.path.join(base_video_dir, video_name, f"{frame_names[input_frame_idx]}.jpg")))
+        # show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
+        # print(out_mask_logits.shape)
         
-        # Save the visualization image
-        vis_path = os.path.join(output_mask_dir, f"vis_add_mask_{frame_names[input_frame_idx]}.png")
-        plt.savefig(vis_path)
-        plt.close()  # Close the figure to free memory
+        # # Save the visualization image
+        # vis_path = os.path.join(output_mask_dir, f"vis_add_mask_{frame_names[input_frame_idx]}.png")
+        # plt.savefig(vis_path)
+        # plt.close()  # Close the figure to free memory
         
         #-----------------Save Images - End-------------------------
     return  out_obj_ids, out_mask_logits, object_ids_set
@@ -573,36 +573,36 @@ def add_point(input_mask_dir,output_mask_dir,base_video_dir, video_name, frame_n
     
     #------------------Save Images------------------------------
     
-    os.makedirs(output_mask_dir, exist_ok=True)
-    plt.figure(figsize=(9, 6))
-    plt.title(f"frame {input_frame_idx}")
-    plt.imshow(Image.open(os.path.join(base_video_dir, video_name, f"{frame_names[input_frame_idx]}.jpg")))
+    # os.makedirs(output_mask_dir, exist_ok=True)
+    # plt.figure(figsize=(9, 6))
+    # plt.title(f"frame {input_frame_idx}")
+    # plt.imshow(Image.open(os.path.join(base_video_dir, video_name, f"{frame_names[input_frame_idx]}.jpg")))
     
-    # Overlay the ground truth mask in green
-    gt_mask = gt[input_frame_idx]
-    gt_mask = np.squeeze(gt_mask)  # Ensure it's 2D
-    green_overlay = np.zeros((gt_mask.shape[0], gt_mask.shape[1], 4))
-    green_overlay[..., 1] = 1.0  # green channel
-    green_overlay[..., 3] = gt_mask * 0.4  # alpha based on mask
-    plt.imshow(green_overlay)
+    # # Overlay the ground truth mask in green
+    # gt_mask = gt[input_frame_idx]
+    # gt_mask = np.squeeze(gt_mask)  # Ensure it's 2D
+    # green_overlay = np.zeros((gt_mask.shape[0], gt_mask.shape[1], 4))
+    # green_overlay[..., 1] = 1.0  # green channel
+    # green_overlay[..., 3] = gt_mask * 0.4  # alpha based on mask
+    # plt.imshow(green_overlay)
     
-    # Show the predicted mask on top
-    show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
+    # # Show the predicted mask on top
+    # show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
     
-    # Mark the blob centers on the image with a cross
-    for center in blob_centers:
-        plt.plot(center[0], center[1], 'rx')  # 'rx' for red crosses
+    # # Mark the blob centers on the image with a cross
+    # for center in blob_centers:
+    #     plt.plot(center[0], center[1], 'rx')  # 'rx' for red crosses
     
-    print(out_mask_logits.shape)
+    # print(out_mask_logits.shape)
     
-    # Save the visualization image
-    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    if labels == 1:
-        vis_path = os.path.join(output_mask_dir, f"vis_add_positive_{frame_names[input_frame_idx]}_{center[0]:.3f}_{center[1]:.3f}_{timestamp}.png")
-    elif labels == 0:
-        vis_path = os.path.join(output_mask_dir, f"vis_add_negative_{frame_names[input_frame_idx]}_{center[0]:.3f}_{center[1]:.3f}_{timestamp}.png")
-    plt.savefig(vis_path)
-    plt.close()  # Close the figure to free memory
+    # # Save the visualization image
+    # timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    # if labels == 1:
+    #     vis_path = os.path.join(output_mask_dir, f"vis_add_positive_{frame_names[input_frame_idx]}_{center[0]:.3f}_{center[1]:.3f}_{timestamp}.png")
+    # elif labels == 0:
+    #     vis_path = os.path.join(output_mask_dir, f"vis_add_negative_{frame_names[input_frame_idx]}_{center[0]:.3f}_{center[1]:.3f}_{timestamp}.png")
+    # plt.savefig(vis_path)
+    # plt.close()  # Close the figure to free memory
     
     #-----------------Save Images - End-------------------------
     return out_obj_ids,out_mask_logits                     
@@ -620,37 +620,37 @@ def add_box(input_mask_dir,output_mask_dir,base_video_dir, video_name, frame_nam
     
     #------------------Save Images------------------------------
     
-    os.makedirs(output_mask_dir, exist_ok=True)
-    plt.figure(figsize=(9, 6))
-    plt.title(f"frame {input_frame_idx}")
-    plt.imshow(Image.open(os.path.join(base_video_dir, video_name, f"{frame_names[input_frame_idx]}.jpg")))
+    # os.makedirs(output_mask_dir, exist_ok=True)
+    # plt.figure(figsize=(9, 6))
+    # plt.title(f"frame {input_frame_idx}")
+    # plt.imshow(Image.open(os.path.join(base_video_dir, video_name, f"{frame_names[input_frame_idx]}.jpg")))
     
-    # Overlay the ground truth mask in green
-    gt_mask = gt[input_frame_idx]
-    gt_mask = np.squeeze(gt_mask)  # Ensure it's 2D
-    green_overlay = np.zeros((gt_mask.shape[0], gt_mask.shape[1], 4))
-    green_overlay[..., 1] = 1.0  # green channel
-    green_overlay[..., 3] = gt_mask * 0.4  # alpha based on mask
-    plt.imshow(green_overlay)
+    # # Overlay the ground truth mask in green
+    # gt_mask = gt[input_frame_idx]
+    # gt_mask = np.squeeze(gt_mask)  # Ensure it's 2D
+    # green_overlay = np.zeros((gt_mask.shape[0], gt_mask.shape[1], 4))
+    # green_overlay[..., 1] = 1.0  # green channel
+    # green_overlay[..., 3] = gt_mask * 0.4  # alpha based on mask
+    # plt.imshow(green_overlay)
     
-    # Show the predicted mask on top
-    show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
+    # # Show the predicted mask on top
+    # show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
     
-    # Mark the blob centers on the image with a cross
-    x_min, y_min, x_max, y_max = bbox[0]
-    width = x_max - x_min
-    height = y_max - y_min
-    rect = Rectangle((x_min, y_min), width, height, linewidth=2, edgecolor='r', facecolor='none')
-    plt.gca().add_patch(rect)
+    # # Mark the blob centers on the image with a cross
+    # x_min, y_min, x_max, y_max = bbox[0]
+    # width = x_max - x_min
+    # height = y_max - y_min
+    # rect = Rectangle((x_min, y_min), width, height, linewidth=2, edgecolor='r', facecolor='none')
+    # plt.gca().add_patch(rect)
     
-    print(out_mask_logits.shape)
+    # print(out_mask_logits.shape)
     
-    # Save the visualization image
-    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    if labels == 1:
-        vis_path = os.path.join(output_mask_dir, f"vis_add_box_{frame_names[input_frame_idx]}_{x_min:.3f}_{y_min:.3f}_{x_max:.3f}_{y_max:.3f}_{timestamp}.png")
-    plt.savefig(vis_path)
-    plt.close()  # Close the figure to free memory
+    # # Save the visualization image
+    # timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    # if labels == 1:
+    #     vis_path = os.path.join(output_mask_dir, f"vis_add_box_{frame_names[input_frame_idx]}_{x_min:.3f}_{y_min:.3f}_{x_max:.3f}_{y_max:.3f}_{timestamp}.png")
+    # plt.savefig(vis_path)
+    # plt.close()  # Close the figure to free memory
     
     #-----------------Save Images - End-------------------------
     return out_obj_ids,out_mask_logits  
@@ -973,46 +973,46 @@ def vos_inference(
             confidence_scores[out_frame_idx] = object_score_logits.to(torch.float32).cpu().numpy()
           
         #---------------------------------Save Prediction--------------------------------------  
-        vis_frame_stride = 1   
-        for out_frame_idx in range(input_frame_inds[0], len(frame_names), vis_frame_stride):
-            frame_name = frame_names[out_frame_idx]
-            # print(frame_name)
-            # print(out_frame_idx)
-            # Load RGB frame
-            img = Image.open(os.path.join(base_video_dir, video_name, f"{frame_name}.jpg"))
+        # vis_frame_stride = 1   
+        # for out_frame_idx in range(input_frame_inds[0], len(frame_names), vis_frame_stride):
+        #     frame_name = frame_names[out_frame_idx]
+        #     # print(frame_name)
+        #     # print(out_frame_idx)
+        #     # Load RGB frame
+        #     img = Image.open(os.path.join(base_video_dir, video_name, f"{frame_name}.jpg"))
 
-            # Load ground truth mask image (you can convert it to grayscale if needed)
-            gt_mask_path = os.path.join(input_mask_dir, video_name,f"{frame_name}.png")
-            gt_mask = Image.open(gt_mask_path).convert("L")  # grayscale mask
+        #     # Load ground truth mask image (you can convert it to grayscale if needed)
+        #     gt_mask_path = os.path.join(input_mask_dir, video_name,f"{frame_name}.png")
+        #     gt_mask = Image.open(gt_mask_path).convert("L")  # grayscale mask
 
-            fig, ax = plt.subplots(figsize=(8, 6))
-            #fig.suptitle(f"Frame {out_frame_idx}", fontsize=14)
+        #     fig, ax = plt.subplots(figsize=(8, 6))
+        #     #fig.suptitle(f"Frame {out_frame_idx}", fontsize=14)
 
-            # Show the input image
-            ax.imshow(img)
-            ax.set_title("Predicted + Ground Truth")
-            ax.axis("off")  
+        #     # Show the input image
+        #     ax.imshow(img)
+        #     ax.set_title("Predicted + Ground Truth")
+        #     ax.axis("off")  
 
-            # Convert ground truth to NumPy and normalize to [0,1]
-            gt_mask_np = np.array(gt_mask) / 255.0
+        #     # Convert ground truth to NumPy and normalize to [0,1]
+        #     gt_mask_np = np.array(gt_mask) / 255.0
 
-            # Create transparent green overlay
-            green_overlay = np.zeros((gt_mask_np.shape[0], gt_mask_np.shape[1], 4))
-            green_overlay[..., 1] = 1.0  # green channel
-            green_overlay[..., 3] = gt_mask_np * 0.4  # alpha based on mask
+        #     # Create transparent green overlay
+        #     green_overlay = np.zeros((gt_mask_np.shape[0], gt_mask_np.shape[1], 4))
+        #     green_overlay[..., 1] = 1.0  # green channel
+        #     green_overlay[..., 3] = gt_mask_np * 0.4  # alpha based on mask
 
-            # Overlay ground truth
-            ax.imshow(green_overlay)
+        #     # Overlay ground truth
+        #     ax.imshow(green_overlay)
 
-            # Show predicted masks
-            for out_obj_id, out_mask in video_segments[out_frame_idx].items():
-                show_mask(out_mask, ax, obj_id=out_obj_id)
+        #     # Show predicted masks
+        #     for out_obj_id, out_mask in video_segments[out_frame_idx].items():
+        #         show_mask(out_mask, ax, obj_id=out_obj_id)
 
-            save_path = os.path.join(output_mask_dir, f"{frame_name}_vis.png")
-            plt.tight_layout()
-            plt.savefig(save_path, dpi=150)
+        #     save_path = os.path.join(output_mask_dir, f"{frame_name}_vis.png")
+        #     plt.tight_layout()
+        #     plt.savefig(save_path, dpi=150)
             
-            plt.close(fig) 
+        #     plt.close(fig) 
          #---------------------------------Save Prediction - END --------------------------------------  
         
     predictor.reset_state(inference_state)
@@ -1174,7 +1174,12 @@ def main():
     args = parser.parse_args()
     
     
-
+    if torch.cuda.is_available():
+        print("CUDA is available. Running on GPU:", torch.cuda.get_device_name(0))
+        device = torch.device("cuda")
+    else:
+        print("CUDA is NOT available. Running on CPU.")
+        device = torch.device("cpu")
    
     
     # Add timestamp to the output directory
